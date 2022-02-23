0e6986613d1271d3ae19399e8f51e3ae77f8f374
huang xn <huangxn@milesight.com>
2022-02-23 16:44:10 +0800

    更新init

--- httprunner/__init__.py
+++ httprunner/__init__.py
@@ -7,6 +7,10 @@ from httprunner.parser import parse_parameters as Parameters
 from httprunner.runner import HttpRunner
 from httprunner.testcase import Config, Step, RunRequest, RunTestCase
 
+"""
+若定义了__all__属性，则只有__all__内指定的属性、方法、类可被导入.
+若没定义，则导入模块内的所有公有属性，方法和类。
+"""
 __all__ = [
     "__version__",
     "__description__",
