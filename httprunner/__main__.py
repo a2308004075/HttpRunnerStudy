7599af7d6b09ca7487c50808d79d1cac52cd019b
huang xn <huangxn@milesight.com>
2022-02-23 16:49:48 +0800

    更新main

--- httprunner/__main__.py
+++ httprunner/__main__.py
@@ -1,5 +1,12 @@
 from httprunner.cli import main
 
+"""
+(1) 如果你希望 python 将一个文件夹作为 Package 对待，那么这个文件夹中必须包含一个名为 __init__.py 的文件，即使它是空的。
 
+(2) 如果你需要 python 将一个文件夹作为 Package 执行，那么这个文件夹中必须包含一个名为 __main__.py 的文件。
+
+参考：https://www.cnblogs.com/dangkai/p/10758056.html
+执行方式  python httprunner
+"""
 if __name__ == "__main__":
     main()
